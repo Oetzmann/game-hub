645fb71b6855c6e9b5d8fe0601d641ae6d3075da
Andre Oestreich <mail@andreoestreich.de>
2024-07-07 01:45:54 +0200

    Rafactoring: remove duplicate interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -13,7 +13,7 @@ import { SiNintendo } from "react-icons/si";
 
 import { BsGlobe } from "react-icons/bs";
 
-import { Platform } from "../hooks/useGames";
+import { Platform } from "../hooks/usePlatforms";
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
