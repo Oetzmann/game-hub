86521b62377e570bde2fa41c57dd48876ce2713a
Andre Oestreich <mail@andreoestreich.de>
2024-07-08 08:58:25 +0200

    Build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,21 @@
+import useGameTrailers from "../hooks/useGameTrailers";
+import { Game } from "../entities/Game";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useGameTrailers(gameId);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  const first = data?.results[0];
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls />
+  ) : null;
+};
+
+export default GameTrailer;
---
+++ src/hooks/useGameTrailers.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { GameTrailer } from "../entities/GameTrailer";
+import APIClient from "../services/api-client";
+
+const useGameTrailers = (gameId: number) => {
+  const apiClient = new APIClient<GameTrailer>(`/games/${gameId}/movies`);
+
+  return useQuery({
+    queryKey: ["trailers", gameId],
+    queryFn: apiClient.getAll,
+  });
+};
+
+export default useGameTrailers;
